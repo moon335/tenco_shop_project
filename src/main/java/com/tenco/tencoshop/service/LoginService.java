69cddca1c6bdb6de9af9541a391e6856014cdf91
DESKTOP-8N3GG4O\GGG <jdy1787@naver.com>
2023-04-28 12:44:56 +0900

    수정

--- src/main/java/com/tenco/tencoshop/service/LoginService.java
+++ src/main/java/com/tenco/tencoshop/service/LoginService.java
@@ -21,11 +21,14 @@ public class LoginService {
 	private PasswordEncoder passwordEncoder;
 
 	// 로그인
+
 	@Transactional
 	public LoginResponseDto signIn(LoginResponseDto loginResponseDto) {
 		User userEntity = userRepository.findByPassword(loginResponseDto);
+		if (userEntity == null) {
+			throw new LoginException("아이디 혹은 비밀번호가 틀렸습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		boolean isMatched = passwordEncoder.matches(loginResponseDto.getPassword(), userEntity.getPassword());
-
 		if (isMatched == false) {
 			throw new LoginException("아이디 혹은 비밀번호가 틀렸습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
